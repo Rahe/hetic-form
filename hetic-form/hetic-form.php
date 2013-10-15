382d5a358f0e71dbcb5732a6f82309435fd12f89
Nicolas Juen <njuen@beapi.fr>
2013-10-15 14:03:03 +0200

    Gestion des champs requis
    On gère les champs requis par l'application
    On affiche les messages si besoin

--- hetic-form/hetic-form.php
+++ hetic-form/hetic-form.php
@@ -26,14 +26,44 @@ add_action( 'plugins_loaded', 'hetic_form_init' );
 function hetif_form_init() {
 	// On ajoute le shortcode
 	add_shortcode( 'hetic-form', 'hetic_form_shortcode' );
+
+	// On se place au moment du template_redirect car aucune information n'est encore affichée
+	add_action( 'template_redirect', 'hetic_form_process_form' );
 }
 
 function hetic_form_shortcode() {
+	// init du global des messages pour les remplir au fur et à mesure
+	global $hetic_form_messages;
+
 	// on commence un buffer
 	ob_start();
+
 	// on affiche le fichier hetic-form.php
 	include_once( HETIC_FORM_DIR.'/vues/hetic-form.php' );
 
 	// On récupère le texte et on le retourne !
 	return ob_get_flush();
+}
+
+function hetic_form_process_form() {
+	global $hetic_form_messages;
+
+	// Si on a pas soumis le formulaire alors on ne commence pas à traiter les informations
+	if( !isset( $_POST['hetic_form_submit'] ) ) {
+		return false;
+	}
+
+	if( !isset( $_POST['hetic_form_name'] ) || empty( $_POST['hetic_form_name'] ) ) {
+		$hetic_form_messages .= 'Vous devez remplir votre nom<br/>';
+	}
+
+	if( !isset( $_POST['hetic_form_firstname'] ) || empty( $_POST['hetic_form_firstname'] ) ) {
+		$hetic_form_messages .= 'Vous devez remplir votre prénom<br/>';
+	}
+
+	// S'il y a des messages à afficher, alors on ne continue pas.
+	if( !empty( $hetic_form_messages ) ) {
+		return false;
+	}
+
 }
--- hetic-form/vues/hetic-form.php
+++ hetic-form/vues/hetic-form.php
@@ -1,11 +1,14 @@
 <form action='<?php the_permalink(); ?>' method="POST" >
+	<?php
+	// On affiche les messages
+	echo $hetic_form_messages; ?>
 	<fieldset>
-		<legend>LEs informations personelles</legend>
-		<label>Nom : </label>
+		<legend>Les informations personelles</legend>
+		<label>Nom *: </label>
 		<input type="text" value="" name="hetic_form_name" >
 
-		<label>Prénom : </label>
+		<label>Prénom *: </label>
 		<input type="text" value="" name="hetic_form_firstname" >
 	</fieldset>
-	<input type="submit" value="Envoyer !" />
+	<input type="submit" value="Envoyer !" name="hetic_form_submit" />
 </form>
