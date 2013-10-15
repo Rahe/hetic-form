a09f74be90ea84a5eefe95660c457c0bd0a8f7d9
Nicolas Juen <njuen@beapi.fr>
2013-10-15 13:48:26 +0200

    On crée l'entête du plugin

---
+++ hetic-form/hetic-form.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ Plugin Name: HETIC FORM
+ Version: 0.1
+ Plugin URI: hetic.net
+ Description: Display a form
+ Author: It's me! Mario !
+ Author URI: http://www.hetic.fr
+
+ TODO:
+	* Etape 1 : Création du plugin
+	* Etape 2 : Init du shortcode
+	* Etape 3 : Affichage du formulaire
+	* Etape 4 : Traitement PHP du formulaire ( champs requis etc. )
+	* Etape 5 : Insertion du contenu
+	* Etape 6 : Sécurité
+	* Etape 7 : Images
+ */
